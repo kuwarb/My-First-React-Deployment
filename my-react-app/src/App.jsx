b518be0b0914800428232325d983fa61c3bc3a4e
Kuwar Bagga <kuwarbagga95@gmail.com>
2025-12-06 16:53:19 +0530

    second commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
 
   return (
-   <h1> Hello World! My first React Deployment</h1>
+    <>
+      <h1> Hello World! My first React Deployment</h1>
+      <h2> Second commit to test auto deployment </h2>
+    </>
   )
 }
 
