fb417d6435f37302790e88151ec7dfad07c33307
Kuwar Bagga <kuwarbagga95@gmail.com>
2025-12-10 16:35:57 +0530

    Commit after connection to Backend and DB

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,30 @@
+import { useEffect } from 'react'
 import './App.css'
 
 function App() {
+
+  const [data, setData] = useEffect([]);
+
+  useEffect(() => {
+    fetch('https://my-first-backend-deployment.onrender.com/users')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error))
+  }, [])
 
   return (
     <>
       <h1> Hello World! My first React Deployment</h1>
       <h2> Second commit to test auto deployment </h2>
+      {data.length > 0 ? (
+        <ul>
+          {data.map(user => (
+            <li key={user.id}>{user.name} - {user.id}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>No users found.</p>
+      )}    
     </>
   )
 }
